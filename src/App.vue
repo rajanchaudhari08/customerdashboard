076fbab2e457eb089867179d85a6b62b15081ca9
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-01-27 23:54:55 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="bg-gray-800 p-6">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center">
+        <router-link to="/" class="text-white text-lg font-medium">
+          Icon
+        </router-link>
+      </div>
+      <div class="block lg:hidden">
+        <button
+          @click="isOpen = !isOpen"
+          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        >
+          <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div :class="{ block: isOpen, hidden: !isOpen }">
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              to="/about"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              About
+            </router-link>
+            <router-link
+              to="/contact"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Contact
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+};
+</script>
